372174a6197ea72073d16fa2d5b0a5e7e77b248a
aldhykohar <aldhykohar@gmail.com>
2020-12-22 20:55:55 +0800

    Show Data To RecyclerView

--- app/src/main/java/com/stimednp/mvvmjava/activities/MainActivity.java
+++ app/src/main/java/com/stimednp/mvvmjava/activities/MainActivity.java
@@ -1,34 +1,53 @@
 package com.stimednp.mvvmjava.activities;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.stimednp.mvvmjava.R;
+import com.stimednp.mvvmjava.adapters.TVShowAdapter;
+import com.stimednp.mvvmjava.databinding.ActivityMainBinding;
+import com.stimednp.mvvmjava.model.TVShow;
 import com.stimednp.mvvmjava.viewmodels.MostPopularTVShowsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding activityMainBinding;
     private MostPopularTVShowsViewModel viewModel;
+    private List<TVShow> tvShows = new ArrayList<>();
+    private TVShowAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
-        getMostPopularTVShows();
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        doInitialization();
+    }
 
-        Toast.makeText(this, "Add Toast", Toast.LENGTH_SHORT).show();
+    private void doInitialization() {
+        activityMainBinding.tvShowsRecyclerView.setHasFixedSize(true);
+        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        adapter = new TVShowAdapter(tvShows);
+        activityMainBinding.tvShowsRecyclerView.setAdapter(adapter);
+        getMostPopularTVShows();
     }
 
     private void getMostPopularTVShows() {
-        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse ->
-                Toast.makeText(getApplicationContext(),
-                        "Total Pages : " + mostPopularTVShowsResponse.getTotalPages(),
-                        Toast.LENGTH_SHORT
-                ).show()
+        activityMainBinding.setIsLoading(true);
+        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse -> {
+                    activityMainBinding.setIsLoading(false);
+                    if (mostPopularTVShowsResponse != null) {
+                        if (mostPopularTVShowsResponse.getTvShows() != null) {
+                            tvShows.addAll(mostPopularTVShowsResponse.getTvShows());
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                }
         );
     }
 }
